b43c799e50e3476aab823500b1682a81c4bf3580
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-27 13:58:12 +0400

    added k8s things cicd also

--- src/main/java/org/example/repository/InventoryRepository.java
+++ src/main/java/org/example/repository/InventoryRepository.java
@@ -4,7 +4,6 @@ import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.example.entity.Inventory;
 
-import java.util.List;
 
 @ApplicationScoped
 public class InventoryRepository implements PanacheRepository<Inventory> {
