b851ca4e84afdcf4534804246813e89d28761b15
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-27 22:12:05 +0400

    ade

--- src/main/java/org/example/service/InventoryService.java
+++ src/main/java/org/example/service/InventoryService.java
@@ -28,6 +28,7 @@ public class InventoryService {
     public InventoryResponseDTO addInventory(InventoryRequestDTO inventoryRequestDTO) {
         Inventory inventory = inventoryMapper.toEntity(inventoryRequestDTO);
         inventoryRepository.persistAndFlush(inventory);
+        System.out.println(inventory.getId());
         return inventoryMapper.toDTO(inventory);
     }
 }
