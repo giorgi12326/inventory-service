cc630af2dff6d5f735881582316c2637252095f0
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-28 11:51:33 +0400

    added

--- src/main/java/org/example/dto/UpdateQuantityFromInventory.java
+++ src/main/java/org/example/dto/UpdateQuantityFromInventory.java
@@ -1,8 +1,10 @@
 package org.example.dto;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class UpdateQuantityFromInventory {
     Long productId;
     Integer quantity;
