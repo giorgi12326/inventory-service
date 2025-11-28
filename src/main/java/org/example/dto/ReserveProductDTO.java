cd84c3f189360d5fc5f9f5b8d889c6283f0fe31e
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-11-28 15:30:20 +0400

    as

--- src/main/java/org/example/dto/ReserveProductDTO.java
+++ src/main/java/org/example/dto/ReserveProductDTO.java
@@ -1,10 +1,14 @@
 package org.example.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReserveProductDTO {
     Long productId;
     Integer quantity;
