cce993460092b23828f4c53dcd2e94b5604bc645
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-12-01 10:30:28 +0400

    created inventory first

--- src/main/java/org/example/config/Initializer.java
+++ src/main/java/org/example/config/Initializer.java
@@ -7,9 +7,11 @@ import jakarta.inject.Singleton;
 import jakarta.transaction.Transactional;
 import org.example.dto.Event;
 import org.example.dto.EventType;
+import org.example.dto.InventoryRequestDTO;
 import org.example.dto.ProductInfoDTO;
 import org.example.entity.ProductInfo;
 import org.example.repository.ProductInfoRepository;
+import org.example.service.InventoryService;
 import org.example.service.ProductService;
 
 import java.time.Instant;
@@ -19,26 +21,35 @@ public class Initializer {
     @Inject
     ProductService productService;
 
+    @Inject
+    InventoryService inventoryService;
+
     @Transactional
     void onStart(@Observes StartupEvent ev) {
+        InventoryRequestDTO inventoryRequestDTO = InventoryRequestDTO.builder()
+                .location("Giorgi's Inventory")
+                .build();
+        inventoryService.addInventory(inventoryRequestDTO);
 
         ProductInfoDTO productInfo = ProductInfoDTO.builder()
                 .productId(1L)
                 .quantity(120)
+                .inventoryId(1L)
                 .build();
         productService.addProduct(productInfo);
 
         ProductInfoDTO productInfo1 = ProductInfoDTO.builder()
                 .productId(2L)
                 .quantity(40)
+                .inventoryId(1L)
                 .build();
         productService.addProduct(productInfo1);
 
         ProductInfoDTO productInfo2 = ProductInfoDTO.builder()
                 .productId(3L)
                 .quantity(30)
+                .inventoryId(1L)
                 .build();
         productService.addProduct(productInfo2);
-
     }
 }
