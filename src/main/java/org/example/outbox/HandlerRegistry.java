cb0f7f109433ec1ca86fb7615897f67816e50336
Tsintsadze <giorgi.tsintsadze@exxeta.com>
2025-12-02 17:44:53 +0400

    souting

--- src/main/java/org/example/outbox/HandlerRegistry.java
+++ src/main/java/org/example/outbox/HandlerRegistry.java
@@ -21,6 +21,9 @@ public class HandlerRegistry {
     @Inject
     ProductReleaseHandler productReleaseHandler;
 
+    @Inject
+    ProductCreateHandler productCreateHandler;
+
 
     @PostConstruct
     void init() {
@@ -28,6 +31,7 @@ public class HandlerRegistry {
         handlers.put("PRODUCT_UPDATE", productUpdateHandler);
         handlers.put("PRODUCTS_RELEASE", productReleaseHandler);
         handlers.put("PRODUCT_CREATE", productReleaseHandler);
+        System.out.println(handlers);
     }
 
     public EventHandler getHandler(String eventType) {
